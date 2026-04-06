cbd543aad7908febe3a1807d8ff7b9086c36e589
Eduardo <edunogueira06@hotmail.com>
2026-04-06 18:26:55 -0300

    Ex001 herança

---
+++ Repositories/09HerancaEPolimorfismo/Ex001/src/BrownieDoceDeLeite.java
@@ -0,0 +1,12 @@
+public class BrownieDoceDeLeite extends Brownie{
+
+
+    // Métodos
+    public BrownieDoceDeLeite(String nome, double preco, String sabor){
+        super(nome,preco,sabor);
+    };
+
+    public void adicionaDoceDeLeite(){
+        System.out.println("Doce de leite foi adicionado no brownie.");
+    };
+}
---
+++ Repositories/09HerancaEPolimorfismo/Ex001/src/BrownieNutella.java
@@ -0,0 +1,12 @@
+public class BrownieNutella extends Brownie{
+
+
+    // Métodos
+    public BrownieNutella(String nome, double preco, String sabor){
+        super(nome,preco,sabor);
+    };
+
+    public void adicionaNutella(){
+        System.out.println("Nutella foi adicionada no brownie.");
+    };
+}
